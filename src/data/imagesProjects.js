b4d545a975ebbe48423ec2007aa87a76dcf9619b
Elias Cando <ecando@corporacionlanec.com>
2026-02-02 11:06:07 -0500

    feat: inclusion de proyecto liyanest

--- src/data/imagesProjects.js
+++ src/data/imagesProjects.js
@@ -2,12 +2,14 @@ import blogExpressHome from '../assets/blogExpressHome.jpeg'
 import chatExpressHome from '../assets/chatExpressHome.jpeg'
 import climaExpressHome from '../assets/climaExpressHome.png'
 import botExpressHome from '../assets/botExpressHome.jpeg'
+import liyanestHome from '../assets/liyanestHome.jpeg'
 
 const imagesMap = {
     'blogExpressHome.jpeg': blogExpressHome,
     'chatExpressHome.jpeg': chatExpressHome,
     'climaExpressHome.png': climaExpressHome,
-    'botExpressHome.jpeg': botExpressHome
+    'botExpressHome.jpeg': botExpressHome,
+    'liyanestHome.jpeg': liyanestHome
 };
 
 export default imagesMap;
